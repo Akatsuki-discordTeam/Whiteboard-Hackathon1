0710f25e669d63b1d3607b8ba06e6e9ed8ea39df
Rex-Arnab <khanp4397@gmail.com>
2021-07-05 20:54:44 +0530

    Remaining uploads

--- downloadCanvas.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('download').addEventListener('click', function() {
-    downloadCanvas(this, 'canvas', 'media.png');
-}, false);
-
-function downloadCanvas(link, canvas, filename) {
-    link.href = document.getElementById(canvas).toDataURL();
-    link.download = filename;
-}
